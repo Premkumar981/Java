a27578a42d9c897a536d70c7949ded6999b48424
Suru Prem Kumar <118878059+Premkumar981@users.noreply.github.com>
2024-02-18 15:59:16 +0530

    Add files via upload

---
+++ XOR_operations.java
@@ -0,0 +1,14 @@
+public class XOR_operations {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 7;
+
+        // String binaryCode1 = "1010";
+        // String binaryCode2 = "0101";
+        // int result = Integer.parseInt(binaryCode1) ^ Integer.parseInt(binaryCode2);
+
+        int result = a ^ b;
+
+        System.out.println(result);
+    }
+}
