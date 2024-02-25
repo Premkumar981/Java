870ae47eaac0e41a83fb0272ea593aa7290e9dfa
Suru Prem Kumar <118878059+Premkumar981@users.noreply.github.com>
2024-02-25 21:52:21 +0530

    Add files via upload

---
+++ Arithmetic_Operators.java
@@ -0,0 +1,11 @@
+public class Arithmetic_Operators {
+    public static void main(String[] args) {
+		int x = 16;
+		int y = 3;
+		System.out.println("x + y = " + (x + y));
+		System.out.println("x - y = " + (x - y));
+		System.out.println("x * y = " + (x * y));
+		System.out.println("x / y = " + (x / y));
+		System.out.println("x % y = " + (x % y));
+	}
+}
