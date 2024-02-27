ad4eddac198e401a3bc4bddae507c70910096f68
Suru Prem Kumar <118878059+Premkumar981@users.noreply.github.com>
2024-02-27 16:48:36 +0530

    Add files via upload

---
+++ TernaryOperator.java
@@ -0,0 +1,8 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+		int javaCertificationMarks = 75;
+		int passMarks = 65;
+		String result = (javaCertificationMarks > passMarks) ? "Passed Java Certification" : "Failed Java Certification";
+		System.out.println("result : " + result);
+	}
+}
