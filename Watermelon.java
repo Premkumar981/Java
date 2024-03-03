1402743e8e9c7f21185afbfadd269044734620f3
Suru Prem Kumar <118878059+Premkumar981@users.noreply.github.com>
2024-03-03 11:06:26 +0530

    Add files via upload

---
+++ Watermelon.java
@@ -0,0 +1,15 @@
+//import java.util.Scanner;;
+public class Watermelon {
+    public static void main(String[] args) {
+        
+        int w = 10;
+        //to take input from user
+        //Scanner sc = new Scanner(System.in);
+        //int w = sc.nextInt(); // taken the input
+
+        if(w > 2 && w%2 == 0)
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+    }
+}
