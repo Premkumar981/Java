e892b9de7f0b456fa7f013aa165b3b35b0a482f0
Suru Prem Kumar <118878059+Premkumar981@users.noreply.github.com>
2024-03-04 09:21:24 +0530

    Add files via upload

---
+++ print_1_to_n_reverse.java
@@ -0,0 +1,13 @@
+public class print_1_to_n_reverse {
+    public static void main(String[] args) {
+        
+        //to take input from user
+        //Scanner sc = new Scanner(System.in);
+        //int n = sc.nextInt();
+        int n = 5;
+        for(int ctr = n; ctr >= 1; ctr--) {
+            // 5, 4, 3, 2, 1
+            System.out.println(ctr);
+        }
+    }
+}
