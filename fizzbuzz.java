3b52880df65c3f3cd8150b0c68313ef76009dfdc
Suru Prem Kumar <118878059+Premkumar981@users.noreply.github.com>
2024-03-11 00:08:52 +0530

    Add files via upload

---
+++ fizzbuzz.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class fizzbuzz {
+    public static void main(String[] args) {
+        
+      Scanner sc = new Scanner(System.in);
+
+      int N = sc.nextInt();
+
+      for(int i = 1; i <= N; i++) {
+        if(i%3 == 0 && i%5 == 0)
+            System.out.println("FizzBuzz");
+            else if(i%5 == 0)
+              System.out.println("Buzz");
+            else if(i%3 == 0)
+              System.out.println("Fizz");
+            else
+              System.out.println(i);
+
+      }
+      sc.close();
+    }
+}
