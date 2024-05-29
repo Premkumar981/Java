6fcebd89bdaa32a1d4986ddfb899beca42cb7e14
Suru Prem Kumar <118878059+Premkumar981@users.noreply.github.com>
2024-05-29 20:42:52 +0530

    Add files via upload

---
+++ print_natural_num.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Main {
+    // Will run infinitely if a negative
+    // input is given
+    public static void print1ToN(int n) {
+        if(n == 0)
+            return;
+
+        print1ToN(n-1);
+        System.out.print(" " + n);
+    }
+
+    public static void main(String[] args) {
+        // Note: Don't change class name
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        print1ToN(N);
+    }
+}
