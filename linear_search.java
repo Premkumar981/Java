9f459ec9c99845fb21f97092111edc6fede10b9f
Suru Prem Kumar <118878059+Premkumar981@users.noreply.github.com>
2024-06-14 16:42:43 +0530

    Add files via upload

--- linear_search.java
+++ linear_search.java
@@ -1,34 +1,57 @@
-import java.util.Scanner;
-import java.util.Arrays;
+// Link: https://www.geeksforgeeks.org/problems/searching-a-number0324/1
 
-public class linear_search {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+import java.io.*;
+import java.util.*;
 
-        // size of the array
-        int N = sc.nextInt();
-
-        int arr[] = new int[N];
-
-        for(int i = 0; i < N; ++i) {
-            arr[i] = sc.nextInt();
-        }
-
-        int target_num = sc.nextInt();
-
-
-        // Now, the actual problem starts.
-        boolean ans = false;
-
-        for(int val : arr) {
-            if(val == target_num) {
-                ans = true;
-                break;
+class Main {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            String[] str = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(str[0]);
+            int k = Integer.parseInt(str[1]);
+            str = br.readLine().trim().split(" ");
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(str[i]);
             }
+
+            int ans = new Solution().search(arr, n, k);
+
+            System.out.println(ans);
         }
+    }
+}
 
-        System.out.println(ans);
+// User function Template for Java
 
-        sc.close();
+// Time: O(N)
+// Space: O(LogN)
+
+class Solution {
+    int searchHelper(int arr[], int st, int en, int k) {
+        if(st > en)
+            return -1;
+            
+        if(st == en)
+            return arr[st] == k? st : -1;
+        
+        int mid = (st + en)/2;    
+        int firstHalf = searchHelper(arr, st, mid, k);
+        
+        if(firstHalf != -1)
+            return firstHalf;
+            
+        int secondHalf = searchHelper(arr, mid + 1, en, k);
+        
+        return secondHalf;
+    }
+    
+    public int search(int arr[], int n, int k) {
+        int ans = searchHelper(arr, 0, n - 1, k);
+        return ans == -1? ans : ans + 1; // convert 0-based to 1-based.
     }
 }
