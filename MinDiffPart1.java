004094c3c27fe1694cfee677ec6e5e07db03adc5
Suru Prem Kumar <118878059+Premkumar981@users.noreply.github.com>
2024-06-28 15:44:12 +0530

    Add files via upload

---
+++ MinDiffPart1.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+// Link : https://www.hackerrank.com/challenges/minimum-absolute-difference-in-an-array/problem
+// Time : O(NLogN)
+
+class Result {
+
+    /*
+     * Complete the 'minimumAbsoluteDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int minimumAbsoluteDifference(List<Integer> arr) {
+        // Write your code here
+        Collections.sort(arr);
+        int ans = Integer.MAX_VALUE;
+        for(int i = 0; i + 1 < arr.size(); ++i)
+            ans = Math.min(ans, arr.get(i + 1) - arr.get(i));
+        return ans;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+            int result = Result.minimumAbsoluteDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
